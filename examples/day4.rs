d94287a190134c1eee8c7ed3ac091a2ecc505b90
Roman Kotelnikov <roman.kotelnikov@outlook.com>
2021-01-23 21:39:12 +0900

    Simplify parsing; partial day2; pending hair color validation

--- examples/day4.rs
+++ examples/day4.rs
@@ -1,17 +1,119 @@
 use itertools::Itertools;
 
-struct Passport<'a> {
-    byr: Option<&'a str>,
-    iyr: Option<&'a str>,
-    eyr: Option<&'a str>,
-    hgt: Option<&'a str>,
-    hcl: Option<&'a str>,
-    ecl: Option<&'a str>,
-    pid: Option<&'a str>,
-    cid: Option<&'a str>,
+enum Height {
+    Cm(u16),
+    Inches(u16),
 }
 
-impl<'a> Passport<'a> {
+impl Height {
+    fn from_str(str: &str) -> Option<Self> {
+        // todo: get rid of String
+        let value = str.chars().take_while(|c| c.is_ascii_digit()).collect::<String>().parse::<u16>().ok()?;
+        match str.chars().skip_while(|c| c.is_ascii_digit()).collect::<String>().as_str() {
+            "cm" if value >= 150 && value <= 193 => Some(Height::Cm(value)),
+            "in" if value >= 59 && value <= 76 => Some(Height::Inches(value)),
+            _ => None
+        }
+    }
+}
+
+struct BirthYear(u16);
+
+// TODO: use generic to validate year
+// fn validate_year<T>(str: &str, from: u16, to: u16) -> Option<T> {
+//
+// }
+
+impl BirthYear {
+    fn from_str(str: &str) -> Option<Self> {
+        str.parse::<u16>()
+            .ok()
+            .filter(|&value| value >= 1920 && value <= 2002)
+            .map(|value| BirthYear(value))
+    }
+}
+
+struct IssueYear(u16);
+
+impl IssueYear {
+    fn from_str(str: &str) -> Option<Self> {
+        str.parse::<u16>()
+            .ok()
+            .filter(|&value| value >= 2010 && value <= 2020)
+            .map(|value| IssueYear(value))
+    }
+}
+
+
+struct ExpireYear(u16);
+
+impl ExpireYear {
+    fn from_str(str: &str) -> Option<Self> {
+        str.parse::<u16>()
+            .ok()
+            .filter(|&value| value >= 2020 && value <= 2030)
+            .map(|value| ExpireYear(value))
+    }
+}
+
+struct HairColor([char; 1]);
+
+impl HairColor {
+    fn from_str(str: &str) -> Option<Self> {
+        // str.matches("[0-9a-f]{6}")
+        Some(HairColor(['a']))
+    }
+}
+
+enum EyeColor {
+    Amb,
+    Blu,
+    Brn,
+    Gry,
+    Grn,
+    Hzl,
+    Oth,
+}
+
+impl EyeColor {
+    fn from_str(str: &str) -> Option<Self> {
+        use EyeColor::*;
+        match str {
+            "amb" => Some(Amb),
+            "blu" => Some(Blu),
+            "brn" => Some(Brn),
+            "gry" => Some(Gry),
+            "grn" => Some(Grn),
+            "hzl" => Some(Hzl),
+            "oth" => Some(Oth),
+            _ => None
+        }
+    }
+}
+
+struct PassportID(u32);
+
+impl PassportID {
+    fn from_str(str: &str) -> Option<Self> {
+        if str.chars().take_while(|c| c.is_ascii_digit()).count() == 9 {
+            Some(PassportID(str.parse::<u32>().ok()?))
+        } else {
+            None
+        }
+    }
+}
+
+struct Passport {
+    byr: Option<BirthYear>,
+    iyr: Option<IssueYear>,
+    eyr: Option<ExpireYear>,
+    hgt: Option<Height>,
+    hcl: Option<HairColor>,
+    ecl: Option<EyeColor>,
+    pid: Option<PassportID>,
+}
+
+impl Passport {
     fn new() -> Self {
         Passport {
             byr: None,
@@ -21,7 +123,6 @@ impl<'a> Passport<'a> {
             hcl: None,
             ecl: None,
             pid: None,
-            cid: None
         }
     }
 
@@ -35,53 +136,40 @@ impl<'a> Passport<'a> {
             self.pid.is_some()
     }
 
-    fn from_new_line(l: &'a str) -> Self {
-        Passport::new().add_line(l)
+    fn from_new_line(l: &str) -> Self {
+        l
+            .split(&[' ', '\n'][..])
+            .filter(|s| !s.is_empty())
+            .fold(Passport::new(), |mut p, next| {
+                // todo: fix unwrap
+                let (f, value) = next.split(':').next_tuple().unwrap();
+
+                match f {
+                    "byr" => p.byr = BirthYear::from_str(value),
+                    "iyr" => p.iyr = IssueYear::from_str(value),
+                    "eyr" => p.eyr = ExpireYear::from_str(value),
+                    "hgt" => p.hgt = Height::from_str(value),
+                    "hcl" => p.hcl = HairColor::from_str(value),
+                    "ecl" => p.ecl = EyeColor::from_str(value),
+                    "pid" => p.pid = PassportID::from_str(value),
+                    _ => (),
+                };
+
+                p
+            })
     }
 
-    fn add_line(mut self, l: &'a str) -> Self {
-        l.split(' ').fold(self, |mut p , next| {
-            // todo: fix unwrap
-            let (f, value) = next.split(':').next_tuple().unwrap();
-
-            match f {
-                "byr" => p.byr = Some(value),
-                "iyr" => p.iyr = Some(value),
-                "eyr" => p.eyr = Some(value),
-                "hgt" => p.hgt = Some(value),
-                "hcl" => p.hcl = Some(value),
-                "ecl" => p.ecl = Some(value),
-                "pid" => p.pid = Some(value),
-                "cid" => p.cid = Some(value),
-                _ => (),
-            };
-
-            p
-        })
-    }
-
-    fn from_lines(lines: impl Iterator<Item=&'a str>) -> Vec<Passport<'a>> {
-        lines.into_iter().fold(
-            (Vec::new(), None),
-            |(mut v, mut current): (Vec<Passport>, Option<Passport>), next| {
-                match current.take() {
-                    Some(p) if next.is_empty() => {
-                        if p.is_valid() {
-                            v.push(p)
-                        };
-                        (v, None)
-                    },
-                    Some(mut p) => (v, Some(p.add_line(next))),
-                    None => (v, Some(Passport::from_new_line(next))),
-                }
-        }).0
+    fn from_lines<'a>(lines: impl Iterator<Item=&'a str>) -> Vec<Passport> {
+        lines.into_iter().map(
+            |next| Passport::from_new_line(next)
+        ).collect()
     }
 }
 
 fn main() {
     let inputs = std::fs::read_to_string("inputs/input4").unwrap();
 
-    let valid_passports = Passport::from_lines(inputs.split('\n'))
+    let valid_passports = Passport::from_lines(inputs.split("\n\n"))
         .into_iter()
         .filter(|p| p.is_valid())
         .count();
