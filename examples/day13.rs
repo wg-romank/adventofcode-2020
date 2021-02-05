272903f33905597bb92b6eb3565b4789ea28b6cf
Roman Kotelnikov <roman.kotelnikov@outlook.com>
2021-02-05 21:25:20 +0900

    Refactor day13 a bit

--- examples/day13.rs
+++ examples/day13.rs
@@ -16,30 +16,22 @@ fn main() {
             .collect::<Vec<(usize, u64)>>()
     ).unwrap();
 
-    let result = busses
+    let result_p1 = busses
         .iter()
         .map(|&(_, b)| (b, b - ts % b))
         .sorted_by(|a, b| a.1.cmp(&b.1)).next();
+    println!("result {:#?}", result_p1);
 
-    println!("ts {:#?}", ts);
-    println!("result {:#?}", result);
-
-    let items = busses
+    let result_p2 = busses
         .iter()
         .map(|&(idx, b)| ((b - (idx as u64 % b)) % b, b))
         .sorted_by(|a, b| b.1.cmp(&a.1))
-        .collect::<Vec<(u64, u64)>>();
-
-    let result = items
-        .iter()
-        .skip(1)
-        .fold(items[0], |(acc, v): (u64, u64), &(ai, ni): &(u64, u64)| {
+        .fold((0, 1), |(acc, v): (u64, u64), (ai, ni): (u64, u64)| {
             let mut i = 1;
             while (acc + i * v) % ni != ai {
                 i += 1;
             };
             (acc + i * v, v * ni)
         });
-
-    println!("ts2 {:#?}", result);
+    println!("result2 {:#?}", result_p2);
 }
