3c39c95e4a0fbf5af981d7014d292baafefc4582
Roman Kotelnikov <roman.kotelnikov@outlook.com>
2021-06-22 23:00:12 +0900

    Prettify 5

--- examples/day5.rs
+++ examples/day5.rs
@@ -1,41 +1,50 @@
-use std::collections::HashSet;
+use itertools::sorted;
 
-fn binary_search(row: &str, low: u16, high: u16) -> u16 {
+fn binary_search(row: &str, low: u16, high: u16) -> Option<u16> {
     row.chars()
-        .fold((low, high), |(low, high), c| match c {
-            'F' | 'L' => (low, (low + high) / 2),
-            'B' | 'R' => ((low + high) / 2 + 1, high),
-            _ => panic!("Malformed row {}", row),
+        .fold(Some((low, high)), |acc, c| match acc {
+            Some((low, high)) => match c {
+                'F' | 'L' => Some((low, (low + high) / 2)),
+                'B' | 'R' => Some(((low + high) / 2 + 1, high)),
+                _ => None,
+            },
+            None => None,
         })
-        .0
+        .map(|(result, _)| result)
 }
 
-fn seat_id(boarding: &str) -> u16 {
-    let (row, seat) = boarding.split_at(7);
+fn seat_id(boarding: &str) -> Option<u16> {
+    if boarding.len() > 7 {
+        let (row, seat) = boarding.split_at(7);
 
-    let row = binary_search(row, 0, 127);
-    let col = binary_search(seat, 0, 7);
+        let row = binary_search(row, 0, 127)?;
+        let col = binary_search(seat, 0, 7)?;
 
-    row * 8 + col
+        Some(row * 8 + col)
+    } else {
+        None
+    }
 }
 
 fn main() {
-    let inputs = std::fs::read_to_string("inputs/input5").unwrap();
+    let inputs = std::fs::read_to_string("inputs/input5").expect("no input file");
 
-    let mut ids = inputs
-        .split('\n')
-        .filter(|&str| !str.is_empty())
-        .map(seat_id)
-        .collect::<Vec<u16>>();
-    ids.sort();
+    let ids = sorted(inputs.split('\n').flat_map(seat_id)).collect::<Vec<u16>>();
 
-    let max_id = ids.last();
-    println!("max_id {:#?}", max_id);
+    let first_id_option = ids.first().map(|v| *v);
+    let last_id_option = ids.last().map(|v| *v);
 
-    let r = (*ids.first().unwrap()..*ids.last().unwrap());
-    let idd = ids.into_iter().collect::<HashSet<u16>>();
+    match (first_id_option, last_id_option) {
+        (Some(first), Some(last)) => {
+            println!("max id {:?}", last);
+            let missing = (first..last)
+                .zip(ids)
+                .skip_while(|(l, r)| l == r)
+                .map(|(l, _)| l)
+                .next();
 
-    let missing = r.filter(|m| !idd.contains(&m)).collect::<Vec<u16>>();
-
-    println!("my id {:#?}", missing);
+            println!("my id {:?}", missing);
+        }
+        _ => (),
+    }
 }
