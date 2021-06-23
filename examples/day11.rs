cd85a9ac0aa3a3422e72aeefb3d34bb15fed9ca2
Roman Kotelnikov <roman.kotelnikov@outlook.com>
2021-06-23 22:56:52 +0900

    Cycle causing endless loop

--- examples/day11.rs
+++ examples/day11.rs
@@ -12,18 +12,23 @@ enum Seat {
 // todo: part2
 fn directions() -> Vec<fn(i64, i64) -> (i64, i64)> {
     vec![
-        |ii, jj| { (ii + 1, jj) },
-        |ii, jj| { (ii - 1, jj) },
-        |ii, jj| { (ii, jj + 1) },
-        |ii, jj| { (ii, jj - 1) },
-        |ii, jj| { (ii + 1, jj + 1) },
-        |ii, jj| { (ii + 1, jj - 1) },
-        |ii, jj| { (ii - 1, jj + 1) },
-        |ii, jj| { (ii - 1, jj - 1) },
+        |ii, jj| (ii + 1, jj),
+        |ii, jj| (ii - 1, jj),
+        |ii, jj| (ii, jj + 1),
+        |ii, jj| (ii, jj - 1),
+        |ii, jj| (ii + 1, jj + 1),
+        |ii, jj| (ii + 1, jj - 1),
+        |ii, jj| (ii - 1, jj + 1),
+        |ii, jj| (ii - 1, jj - 1),
     ]
 }
 
-fn neighboors_pt1(ii: usize, jj: usize, max_i: usize, max_j: usize) -> impl Iterator<Item=(usize, usize)> {
+fn neighboors_pt1(
+    ii: usize,
+    jj: usize,
+    max_i: usize,
+    max_j: usize,
+) -> impl Iterator<Item = (usize, usize)> {
     directions()
         .into_iter()
         .map(move |f| f(ii as i64, jj as i64))
@@ -31,7 +36,38 @@ fn neighboors_pt1(ii: usize, jj: usize, max_i: usize, max_j: usize) -> impl Iter
         .map(|(i, j)| (i as usize, j as usize))
 }
 
-fn step<I: Iterator<Item=(usize, usize)>>(field: &Vec<Vec<Seat>>, neighboors_fn: fn(usize, usize, usize, usize) -> I) -> Vec<(usize, usize, Seat)> {
+fn neighboors_pt2(
+    ii: usize,
+    jj: usize,
+    max_i: usize,
+    max_j: usize,
+) -> impl Iterator<Item = (usize, usize)> {
+    let initials = neighboors_pt1(ii, jj, max_i, max_j).collect::<Vec<(usize, usize)>>();
+
+    directions()
+        .into_iter()
+        .cycle()  // todo: fix endless loop here
+        .scan((0, initials), move |(idx, acc), f: fn(i64, i64) -> (i64, i64)| {
+            if acc.len() > 0 {
+                let (i, j) = acc[*idx];
+                let (ii, jj) = f(i as i64, j as i64);
+                if ii >= 0 && ii < max_i as i64 && jj >= 0 && jj < max_j as i64 {
+                    acc[*idx] = (ii as usize, jj as usize);
+                } else {
+                    acc.remove(*idx);
+                }
+                *idx = (*idx + 1) % acc.len();
+                Some((i, j))
+            } else {
+                None
+            }
+        })
+}
+
+fn step<I: Iterator<Item = (usize, usize)>>(
+    field: &Vec<Vec<Seat>>,
+    neighboors_fn: fn(usize, usize, usize, usize) -> I,
+) -> Vec<(usize, usize, Seat)> {
     field
         .iter()
         .enumerate()
@@ -43,20 +79,35 @@ fn step<I: Iterator<Item=(usize, usize)>>(field: &Vec<Vec<Seat>>, neighboors_fn:
                         // check if 0 neighboors
                         neighboors_fn(idx, idx2, field.len(), row.len())
                             .into_iter()
-                            .fold_while(Some((idx, idx2, Seat::Occupied)), |acc, (i, j)|
-                                if field[i][j] == Seat::Occupied { Done(None) } else { Continue(acc) }
-                            ).into_inner()
-                        },
+                            .fold_while(Some((idx, idx2, Seat::Occupied)), |acc, (i, j)| {
+                                if field[i][j] == Seat::Occupied {
+                                    Done(None)
+                                } else {
+                                    Continue(acc)
+                                }
+                            })
+                            .into_inner()
+                    }
                     Seat::Occupied => {
                         // check if at least 4 neighboors
                         neighboors_fn(idx, idx2, field.len(), row.len())
                             .into_iter()
                             .fold_while((0, None), |(acc, _), (i, j)| {
-                                let new_acc = if field[i][j] == Seat::Occupied { acc + 1 } else { acc };
-                                if new_acc >= 4 { Done((0, Some((idx, idx2, Seat::Free)))) } else { Continue((new_acc, None))}
-                            }).into_inner().1
+                                let new_acc = if field[i][j] == Seat::Occupied {
+                                    acc + 1
+                                } else {
+                                    acc
+                                };
+                                if new_acc >= 4 {
+                                    Done((0, Some((idx, idx2, Seat::Free))))
+                                } else {
+                                    Continue((new_acc, None))
+                                }
+                            })
+                            .into_inner()
+                            .1
                     }
-                } 
+                }
             })
         })
         .collect()
@@ -66,9 +117,12 @@ fn update(field: &mut Vec<Vec<Seat>>, updates: Vec<(usize, usize, Seat)>) {
     updates.into_iter().for_each(|(i, j, s)| field[i][j] = s)
 }
 
-fn play(mut field: Vec<Vec<Seat>>) -> usize {
+fn play<I: Iterator<Item=(usize, usize)>>(
+    mut field: Vec<Vec<Seat>>,
+    neighboors_fn: fn(usize, usize, usize, usize) -> I,
+) -> usize {
     loop {
-        let updates = step(&field, neighboors_pt1);
+        let updates = step(&field, neighboors_fn);
         display(&field);
         if updates.len() == 0 {
             break;
@@ -113,7 +167,15 @@ fn main() {
         })
         .collect::<Vec<Vec<Seat>>>();
 
-    let occupied = play(field);
+    // let occupied = play(field, neighboors_pt1);
+    // println!("occupied {}", occupied);
 
-    println!("occupied {}", occupied);
+    let occupied2 = play(field, neighboors_pt2);
+    println!("occupied2 {}", occupied2);
+}
+
+
+#[test]
+fn test_neighboors_pt2() {
+    neighboors_pt2(3, 3, 6, 6).for_each(|f| println!("{:#?}", f));
 }
