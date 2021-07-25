4cc61f2d32085b4cab93f309e6939a5829c57e47
Roman Kotelnikov <roman.kotelnikov@outlook.com>
2021-07-25 19:42:50 +0900

    Day 10 pt 2

--- examples/day10.rs
+++ examples/day10.rs
@@ -1,37 +1,41 @@
 use itertools::Itertools;
+use std::collections::HashMap;
 
 fn can_connect(jolt1: u32, jolt2: u32) -> bool {
     (jolt1 as i32 - jolt2 as i32).abs() <= 3
 }
 
-// todo: do not count subtrees multiple times
-fn count_ways(adapters: &[u32], idx: usize, target_joltage: u32) -> u32 {
-    if can_connect(adapters[idx], target_joltage) {
-        1
-    } else {
-        let new_idx = idx + 1;
-        adapters[new_idx..]
-            .iter()
-            .enumerate()
-            .take_while(|(_, &ad)| can_connect(ad, adapters[idx]))
-            .map(|(offset, _)| count_ways(&adapters, new_idx + offset, target_joltage))
-            .fold(0, |acc, v| acc + v)
-    }
+fn cw(adapters: Vec<u32>, target_joltage: u32) -> u64 {
+    let m: HashMap<u32, u64> = adapters.iter().rev().fold(HashMap::new(), |mut m, &jolt| {
+        let cc = u64::max(
+            1,
+            (jolt..target_joltage)
+                .filter(|&j| can_connect(j, jolt)) // todo: takewhile?
+                .rev()
+                .flat_map(|j| m.get(&j))
+                .sum::<u64>(),
+        );
+        m.insert(jolt, u64::max(1, cc));
+        m
+    });
+    m[&0]
 }
 
 fn main() {
-    let inputs = std::fs::read_to_string("inputs/input10").unwrap();
+    let inputs = std::fs::read_to_string("inputs/input10").expect("missing input file");
 
-    let adapters = inputs
+    let mut adapters = inputs
         .split('\n')
         .flat_map(|str| str.parse::<u32>().ok())
         .sorted()
         .collect::<Vec<u32>>();
+    adapters.insert(0, 0);
 
     let target_joltage = adapters
         .iter()
-        .fold(u32::min_value(), |acc, &v| if v >= acc { v } else { acc })
-        + 3;
+        .max()
+        .map(|&j| j + 3)
+        .expect("empty adapters list?");
 
     let (j1diff, j3diff, _) = adapters
         .iter()
@@ -47,8 +51,5 @@ fn main() {
 
     println!("target joltage {}", target_joltage);
 
-    println!(
-        "configurations {}",
-        count_ways(&[&[0], adapters.as_slice()].concat(), 0, target_joltage)
-    )
+    println!("configurations {}", cw(adapters, target_joltage));
 }
