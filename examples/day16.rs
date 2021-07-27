0e4013e553adadcbf472d21dc59138198a6f2ed6
Roman Kotelnikov <roman.kotelnikov@outlook.com>
2021-07-27 16:32:58 +0900

    Day 17 pt2

--- examples/day16.rs
+++ examples/day16.rs
@@ -1,5 +1,6 @@
 use itertools::Itertools;
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct Interval {
@@ -8,12 +9,6 @@ struct Interval {
 }
 
 impl Interval {
-    fn from_usize(value: usize) -> Self {
-        Interval {
-            from: value,
-            to: value,
-        }
-    }
     fn from_str(raw: &str) -> Option<Self> {
         let (l, r) = raw.split('-').map(str::trim).next_tuple()?;
         let from = l.parse::<usize>().ok()?;
@@ -25,64 +20,32 @@ impl Interval {
     fn contains(&self, value: usize) -> bool {
         self.from <= value && value <= self.to
     }
-
-    fn try_append(&mut self, value: usize) -> Option<Self> {
-        if value - 1 <= self.to {
-            self.to = usize::max(self.to, value);
-            None
-        } else {
-            Some(Interval {
-                from: value,
-                to: value,
-            })
-        }
-    }
 }
 
 trait IntervalRange {
-    fn from_vec(values: Vec<usize>) -> Self;
     fn contains(&self, value: usize) -> bool;
 }
 
 impl IntervalRange for Vec<Interval> {
-    fn from_vec(values: Vec<usize>) -> Self {
-        let first = values[0].clone();
-
-        let (mut v, i) = values[1..].into_iter().fold(
-            (vec![], Interval::from_usize(first)),
-            |(mut acc, mut current), &value| {
-                if let Some(new_interval) = current.try_append(value) {
-                    acc.push(current);
-                    (acc, new_interval)
-                } else {
-                    (acc, current)
-                }
-            },
-        );
-
-        v.push(i);
-        v
-    }
-
     fn contains(&self, value: usize) -> bool {
         self.iter().any(|i| i.contains(value))
     }
 }
 
 trait Rules {
-    fn count_error_rate(&self, ticket: &Vec<usize>) -> usize;
-    fn is_ticket_valid(&self, ticket: &Vec<usize>) -> bool;
+    fn count_error_rate(&self, ticket: &[usize]) -> usize;
+    fn is_ticket_valid(&self, ticket: &[usize]) -> bool;
 }
 
 impl<'a, IR: IntervalRange> Rules for HashMap<&'a str, IR> {
-    fn count_error_rate(&self, ticket: &Vec<usize>) -> usize {
+    fn count_error_rate(&self, ticket: &[usize]) -> usize {
         ticket
             .iter()
             .filter(|&v| self.values().all(|rule| !rule.contains(*v)))
             .sum::<usize>()
     }
 
-    fn is_ticket_valid(&self, ticket: &Vec<usize>) -> bool {
+    fn is_ticket_valid(&self, ticket: &[usize]) -> bool {
         self.count_error_rate(ticket) == 0
     }
 }
@@ -116,30 +79,41 @@ fn parse_ticket(ticket: &str) -> Vec<usize> {
 }
 
 fn check_rules_pt1<R: Rules>(rules: &R, tickets: &[Vec<usize>]) -> usize {
-    tickets.into_iter().fold(0_usize, |error_rate, ticket| {
+    tickets.iter().fold(0_usize, |error_rate, ticket| {
         error_rate + rules.count_error_rate(ticket)
     })
 }
 
-fn compute_interval_range_pt2<R: Rules, IR: IntervalRange>(
+fn compute_possible_values_pt2<'a, R, IR>(
     rules: &R,
     tickets: Vec<Vec<usize>>,
-) -> Vec<IR> {
+) -> HashMap<usize, HashSet<&'a str>>
+where
+    R: Rules + Clone + IntoIterator<Item = (&'a str, IR)>,
+    IR: IntervalRange,
+{
     let res: Vec<Vec<usize>> = (0..tickets[1].len()).map(|_| Vec::new()).collect();
-    tickets
+    let possible_values: Vec<Vec<usize>> = tickets
         .into_iter()
-        .filter(|ticket| rules.is_ticket_valid(ticket))
+        .filter(|ticket| rules.is_ticket_valid(&ticket))
         .fold(res, |mut res, ticket| {
             for (idx, value) in ticket.iter().enumerate() {
-                match res[idx].binary_search(value) {
-                    Ok(_) => (),
-                    Err(pos) => res[idx].insert(pos, *value),
-                }
+                res[idx].push(*value);
             }
             res
-        })
+        });
+
+    possible_values
         .into_iter()
-        .map(IntervalRange::from_vec)
+        .map(|values| {
+            rules
+                .clone()
+                .into_iter()
+                .filter(|(_, r)| values.iter().all(|v| r.contains(*v)))
+                .map(|(k, _)| k)
+                .collect()
+        })
+        .enumerate()
         .collect()
 }
 
@@ -148,7 +122,7 @@ fn main() {
 
     let sections = inputs.split("\n\n").collect::<Vec<&str>>();
     let rules = parse_header(sections[0]);
-    let your_ticket = parse_ticket(sections[1]);
+    let your_ticket = parse_ticket(sections[1].split('\n').collect::<Vec<&str>>()[1]);
     let tickets = sections[2]
         .split('\n')
         .map(parse_ticket)
@@ -159,16 +133,27 @@ fn main() {
         check_rules_pt1(&rules, &tickets)
     );
 
-    let ir: Vec<Vec<Interval>> = compute_interval_range_pt2(&rules, tickets);
+    let mut ir = compute_possible_values_pt2(&rules, tickets);
+    let mut indices = vec![];
 
-    println!("{:?}", ir[0]);
-}
+    while let Some((solo_idx, solo_v)) = ir.iter().find(|(_, v)| v.len() == 1) {
+        let tmp = *solo_v.iter().next().unwrap_or(&"");
 
-#[test]
-fn interval_range_from_vec() {
-    let r: Vec<_> = IntervalRange::from_vec(vec![1, 2, 3, 4, 6, 7]);
-    assert_eq!(
-        r,
-        vec![Interval { from: 1, to: 4 }, Interval { from: 6, to: 7 }]
-    );
+        if tmp.starts_with("departure") {
+            indices.push(*solo_idx);
+        }
+
+        for v in ir.values_mut() {
+            v.remove(tmp);
+        }
+    }
+
+    let ans: usize = your_ticket
+        .iter()
+        .enumerate()
+        .filter(|(idx, _)| indices.contains(idx))
+        .map(|(_, v)| v)
+        .product();
+
+    println!("departure values pt2 {:#?}", ans);
 }
